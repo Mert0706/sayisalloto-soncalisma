f12ad3cc9ecff9ed4948399e76d5d6ef8d01de1e
mert.baskaya <mert.baskaya07@gmail.com>
2021-11-20 12:40:14 +0300

    CokluSartOrnek Değişti

--- src/com/bilgeadam/sayisallotoson/CokluSartOrnek.java
+++ src/com/bilgeadam/sayisallotoson/CokluSartOrnek.java
@@ -7,11 +7,18 @@ public class CokluSartOrnek {
         int sayi1 = 23;
         int sayi2 = 35;
 
-        int sayi3 = 23;
+        int sayi3 = 41;
 
         // veya (or) operatörü ||
+        // ve operatörü && ve sartında hepsini sağlaması gereklidir.
         if (sayi3==sayi1 || sayi3==sayi2){
             System.out.println("Tekrarlanan Sayı Var !");
+        }
+
+
+        while (sayi3 != sayi1 && sayi3 != sayi2){
+            System.out.println("Sayi3 Diğerlerine Eşit Değil!");
+            sayi3 = sayi1;
         }
 
     }
