b59bf1d785303bed22e2d93c3a60e32dd8f6e008
mert.baskaya <mert.baskaya07@gmail.com>
2021-11-20 12:12:51 +0300

    Sayısal loto son çalışma

--- src/com/bilgeadam/sayisallotoson/WhileOrnek.java
+++ src/com/bilgeadam/sayisallotoson/WhileOrnek.java
@@ -2,5 +2,26 @@ package com.bilgeadam.sayisallotoson;
 
 public class WhileOrnek {
 
+    //fpr döngüsünden farkı
+    //sayıdan çok duruma bağlı olmasıdır.
+
+    //while döngüsü ile 1'den 10'a kadar merhaba yazdıralım.
+
+    public static void main(String[] args) {
+
+        int sayac = 1;
+
+        while(sayac <=10){
+            System.out.println(sayac+" Merhaba");
+
+            //Genelde bu unutulur.
+            //Program Sonsuz döngüye giriyor.
+
+            sayac = sayac +1;
+
+        }
+
+    }
+
 
 }
